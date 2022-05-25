52d181b6b605274528e8a39e5c49f0e5eb901b14
kevin <Kevin_wang1@epam.com>
2022-05-25 10:33:14 +0800

    write txt

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
+const csv = require("csvtojson");
+const iconv = require("iconv-lite");
+const fs = require("fs");
+
 /**
  * task 1.1
  */
 
 function reverseNumbers(str){
-
     return str.split("").reverse().join("");
 }
-
+console.log(reverseNumbers('12345 678'))
 /**
  * task 1.2
- * 转utf-8
+ * 
  */
-const csv = require("csvtojson");
-// const iconv = require("iconv-lite");
-import iconv from 'iconv-lite'
 
-const csvFilePath = "./static/test.csv";
+const file_path = "./static/2.csv";
 
-async function csvToJson(){
-    const jsonObj = await csv().fromFile(csvFilePath)
+async function csvToJson (file_path) {
+  try {
+    const jsonObj = await csv().fromFile(file_path)
     const jsonStr = JSON.stringify(jsonObj);
-    // // 转buf
     const buf = Buffer.from(jsonStr);
-    // // 处理中文乱码问题
-    const res = iconv.decode(buf, "gbk");
-
-    console.log("=========+++++结果", res)
-    const csvStr = `first_name,last_namejohn,smithjane,smith`;
-    const jsonObj1 = await csv().fromString(csvStr)
-
-    console.log("+++++++++++", jsonObj1);
+    // 处理中文乱码问题
+    const arr = JSON.parse(iconv.decode(buf, 'utf8'));
+    createTxt(arr);
+  } catch (err) {
+    console.log(err)
+  }
 }
 
+// 创建txt文件
+function createTxt (arr) {
+  const txt = fs.createWriteStream('./static/2.txt', {encoding: 'utf8'});
+  arr.forEach(row => {
+    txt.write("\r\n"); // 换行
+    txt.write(JSON.stringify(row));
+  });
+  
+  txt.end();
+}
+// csvToJson(file_path)
 
-csvToJson()
+// ES6
+// import iconv from 'iconv-lite';
+// import fs from 'fs';
